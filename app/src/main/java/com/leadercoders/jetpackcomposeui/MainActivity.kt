800ca4eeb5b2b986a3a54cc3a2d8bdd0f95d5904
Bahar Büşra Atasoy <baharatasoy280@gmail.com>
2026-09-07 12:54:29 +0300

    Ders 1: Proje oluşturuldu.

---
+++ app/src/main/java/com/leadercoders/jetpackcomposeui/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.leadercoders.jetpackcomposeui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.leadercoders.jetpackcomposeui.ders1.SelamlamaEkrani
+import com.leadercoders.jetpackcomposeui.ui.theme.GR01_MYM_M3_JetpackComposeUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GR01_MYM_M3_JetpackComposeUITheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+
+                        SelamlamaEkrani()
+
+
+
+
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/leadercoders/jetpackcomposeui/ders1/D315_İlkKodumuz.kt
@@ -0,0 +1,10 @@
+package com.leadercoders.jetpackcomposeui.ders1
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun SelamlamaEkrani(){
+    Text("Merhaba Dünya! İlk Compose uygulamam çalışıyor \uD83C\uDF89")
+}
