adfd4d6fd9d9a31e6043829b111c777ae2c029b0
Bahar Büşra Atasoy <baharatasoy280@gmail.com>
2026-09-10 12:48:17 +0300

    Ders 2: Ürün Detay Kartı

--- app/src/main/java/com/leadercoders/jetpackcomposeui/MainActivity.kt
+++ app/src/main/java/com/leadercoders/jetpackcomposeui/MainActivity.kt
@@ -7,16 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFrom
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.leadercoders.jetpackcomposeui.ders1.SelamlamaEkrani
-import com.leadercoders.jetpackcomposeui.ders2.D322_TemelDizilimler
-import com.leadercoders.jetpackcomposeui.ders2.D326_ProfilKarti
+import com.leadercoders.jetpackcomposeui.ders2.UrunDetayKarti
 import com.leadercoders.jetpackcomposeui.ui.theme.GR01_MYM_M3_JetpackComposeUITheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +26,8 @@ class MainActivity : ComponentActivity() {
 
 //                       DERS2
  //                       D322_TemelDizilimler()
-                        D326_ProfilKarti()
+  //                      D326_ProfilKarti()
+                        UrunDetayKarti()
 
                     }
 
---
+++ app/src/main/java/com/leadercoders/jetpackcomposeui/ders2/UrunDetayKarti.kt
@@ -0,0 +1,80 @@
+package com.leadercoders.jetpackcomposeui.ders2
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun UrunDetayKarti() {
+
+    Column(//Tüm Ekran
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFEAE1FC))
+            .padding(16.dp)
+    ) {
+
+        Box(//Ürün Görseli
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(350.dp)
+                .background(color = Color.White, shape = RoundedCornerShape(12.dp))
+                .border(width = 1.dp, color = Color.LightGray, shape = RoundedCornerShape(12.dp))
+                .padding(24.dp)
+
+
+        ) {
+
+            Text("\uD83D\uDCF8 Ürün Görseli", color = Color.Black, fontSize = 20.sp)
+
+
+        }
+
+        Spacer(modifier = Modifier
+            .height(16.dp))
+
+
+        Row(//Ürün adı ve fiyat
+
+        ) {
+            Text("Ürün adı: ", color = Color.Black, fontSize = 18.sp, fontWeight = FontWeight.Medium)
+            Text("Akıllı Saat", color = Color.Black, fontSize = 18.sp)
+            Spacer(modifier = Modifier.weight(1f))
+            Text("Fiyat: ", color = Color.Black, fontSize = 18.sp,fontWeight = FontWeight.Medium)
+            Text(
+                "1500 TL", color = Color(
+                    0xFF2E7D32
+                ), fontSize = 18.sp
+            )
+        }
+
+        //Sahte Buton
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            "Sepete Ekle ", modifier = Modifier
+                .background(
+                    color = Color(
+                        0xFF2563EB
+                    ), shape = RoundedCornerShape(8.dp)
+                )
+                .padding(16.dp), color = Color.White
+        )
+    }
+
+}
